63b9dcb049b934c077793efc32d561fa159b62b8
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-04-13 19:15:36 +0300

    version0,77

---
+++ components/Third.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="third">
+
+    <div class="content">
+        <div class="row">
+
+
+    </div>
+  </div>
+  </div>
+  </template>
+  
+  <style>
+  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Montserrat:wght@300;400;600;700&display=swap'); */
+  
+  </style>
